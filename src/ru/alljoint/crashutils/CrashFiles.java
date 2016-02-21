f4a66bab162cedd8656287a67f2f8d4ae5918b7d
Alexey Kurgan <ca@alljoint.ru>
2016-02-21 16:54:25 +0300

    Initial commit

---
+++ src/ru/alljoint/crashutils/CrashFiles.java
@@ -0,0 +1,140 @@
+package ru.alljoint.crashutils;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.util.Random;
+
+public class CrashFiles {
+
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Путь к папке должен быть указан");
+			return;
+		}
+		String sPath = args[0];
+
+		if (sPath == null || sPath.isEmpty()) {
+			System.out.println("Указанный путь не должен быть пустым");
+			return;
+		}
+		
+		File path = new File(sPath);
+		if (!path.isDirectory() || !path.exists()) {
+			System.out.println("Указанный путь должен быть каталогом и должен существовать");
+			return;
+		}
+		
+		try (PrintWriter pw = new PrintWriter("restoreinfo.data", "UTF-8")){
+			checkFiles(path);
+			crackFiles(pw, path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void checkFiles(File path) throws IOException {
+		String[] files = path.list();
+		for(String sFile : files) {
+			File file = new File(path, sFile);
+			if (file.isDirectory())
+				checkFiles(file);
+			long fSize = file.length();
+			try (RandomAccessFile raf = new RandomAccessFile(file, "rws")) {
+				System.out.println(file.getAbsolutePath() + " " + fSize);
+			} catch (FileNotFoundException e) {
+			}
+		}
+	}
+	
+	private static Random random = new Random();
+	private static void crackFiles(PrintWriter writer, File path) throws IOException {
+		String[] files = path.list();
+		for(String sFile : files) {
+			File file = new File(path, sFile);
+			if (file.isDirectory())
+				crackFiles(writer, file);
+			long fSize = file.length();
+			if (fSize <= 4)
+				crackSmallFile(writer, file);
+			else
+				crackFile(writer, file);
+		}
+	}
+	
+	private static void crackSmallFile(PrintWriter writer, File file) throws IOException {
+		long fSize = file.length();
+		long lmd = file.lastModified();
+		if (fSize > 0) {
+			try (RandomAccessFile raf = new RandomAccessFile(file, "rws")) {
+				System.out.println(file.getAbsolutePath() + " " + fSize);
+				writer.print('\"');
+				writer.print(file.getAbsolutePath());
+				writer.print('\"');
+				
+				writer.print('|');
+				writer.print(lmd);
+				
+				int b = -1;
+				int i;
+				for (i = 0; (b = raf.read()) >= 0; i++) {
+					writer.print('|');
+					writer.print(i);
+					writer.print(':');
+					writer.print(b);
+				}
+				raf.seek(0L);
+				for (int j = 0; j < i; j++) {
+					raf.write(random.nextInt(256));
+				}
+				writer.println();
+				writer.flush();
+			} catch (FileNotFoundException e) {}
+		}
+	}
+	
+	private static void crackFile(PrintWriter writer, File file) throws IOException {
+		long fSize = file.length();
+		long lmd = file.lastModified();
+		if (fSize > 0) {
+			try (RandomAccessFile raf = new RandomAccessFile(file, "rws")) {
+				System.out.println(file.getAbsolutePath() + " " + fSize);
+				writer.print('\"');
+				writer.print(file.getAbsolutePath());
+				writer.print('\"');
+
+				writer.print('|');
+				writer.print(lmd);
+
+				long count;
+				if (fSize < 16)
+					count = 1;
+				else if (fSize < 128)
+					count = 2;
+				else if (fSize < 512)
+					count = 4;
+				else
+					count = 64;
+
+				for (long i = 0; i < count; i++) {
+					int nb = random.nextInt(256);
+					long pos = (long) Math.floor(Math.random() * fSize);
+					raf.seek(pos);
+					int b = raf.read();
+					writer.print('|');
+					writer.print(pos);
+					writer.print(':');
+					writer.print(b);
+					
+					raf.seek(pos);
+					raf.write(nb);
+				}
+				writer.println();
+				writer.flush();
+			} catch (FileNotFoundException e) {}	
+		}
+	}
+}
---
+++ src/ru/alljoint/crashutils/RestoreAfterCrash.java
@@ -0,0 +1,61 @@
+package ru.alljoint.crashutils;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestoreAfterCrash {
+
+	private static class Part {
+		public long pos;
+		public int b;
+		
+		public Part(long pos, int b) {
+			this.pos = pos;
+			this.b = b;
+		}
+	}
+	
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Файл с информацией для восстановления должен быть указан");
+			return;
+		}
+		String sResFile = args[0];
+		
+		try (FileInputStream fis = new FileInputStream(sResFile);
+				InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+				BufferedReader br = new BufferedReader(isr, 32768)) {
+			String line;
+			while((line = br.readLine()) != null) {
+				String[] parts = line.split("\\|");
+				String fileName = parts[0].substring(1, parts[0].length()-1);
+				long lmd = Long.parseLong(parts[1]);
+				File file = new File(fileName);
+				
+				List<Part> pl = new ArrayList<Part>(parts.length-1);
+				for (int i = 2; i < parts.length; i++) {
+					String[] nps = parts[i].split("\\:");
+					pl.add(new Part(Long.parseLong(nps[0]), Integer.parseInt(nps[1])));
+				}
+				try (RandomAccessFile raf = new RandomAccessFile(file, "rws")) {
+					for (int i = pl.size()-1; i >= 0; i--) {
+						Part p = pl.get(i);
+						raf.seek(p.pos);
+						raf.write(p.b);
+					}
+				}
+				
+				file.setLastModified(lmd);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
